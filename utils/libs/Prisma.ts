cf1899d93e38e9b808a2d98ad6c05158552d9db4
Oscar Bråberg <oscar.braberg@optiqueue.se>
2022-12-19 00:12:44 +0100

    Prisma env changes

--- utils/libs/Prisma.ts
+++ utils/libs/Prisma.ts
@@ -1,11 +1,10 @@
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: (process.env.NODE_ENV === 'production') ? undefined : ['query', 'error'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
