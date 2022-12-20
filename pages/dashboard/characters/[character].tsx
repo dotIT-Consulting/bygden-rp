6f9e964630abc94eaa95dcb1e48352113fb89671
Raven <theravenblu@gmail.com>
2022-12-20 21:12:15 +0100

    Skeleton loader

--- pages/dashboard/characters/[character].tsx
+++ pages/dashboard/characters/[character].tsx
@@ -3,6 +3,7 @@ import {
 	Grid,
 	Group,
 	Paper,
+	Skeleton,
 	Stack,
 	Text,
 	Title,
@@ -20,85 +21,111 @@ export async function getServerSideProps({ params }: any) {
 	};
 }
 
+const parseJSON = (json: any) => {
+  try {
+    return JSON.parse(json)
+  } catch (error) {
+    console.error(error)
+    return [];
+  }
+}
+
 const Character = ({ pageProps: { id } }: { pageProps: { id: string } }) => {
-	const { data } = useSWR(`/api/bygden/fetch-character-data?citizenid=${id}`);
+	const { data, isLoading } = useSWR(`/api/bygden/fetch-character-data?citizenid=${id}`);
 	const character = data?.character;
 
-	const charInfo = JSON.parse(character?.charinfo);
+	const charInfo = parseJSON(character?.charinfo);
 
 	return (
 		<Container fluid>
 			<Grid>
 				<Grid.Col span={4}>
 					<Paper withBorder radius="md" p="xs">
-						<Group position="left">
+						<Group mb={8} position="left">
 							<IconUser size={24} />
 							<Title order={3} transform="uppercase">
 								info
 							</Title>
 						</Group>
 
-            <Grid grow>
-              <Grid.Col span={4}>
-                <Stack spacing={4} mt={8}>
-                  <Text fz="xs" c="dimmed">
-                    Namn
-                  </Text>
-                  <Text>{`${charInfo.firstname} ${charInfo.lastname}`}</Text>
-                </Stack>
-              </Grid.Col>
+            <Skeleton visible={isLoading}>
+              <Grid grow>
+                <Grid.Col span={4}>
+                  <Stack spacing={4}>
+                    <Text fz="xs" c="dimmed">
+                      Namn
+                    </Text>
+                    <Text>{`${charInfo.firstname} ${charInfo.lastname}`}</Text>
+                  </Stack>
+                </Grid.Col>
 
-              <Grid.Col span={4}>
-                <Stack spacing={4} mt={8}>
-                  <Text fz="xs" c="dimmed">
-                    Telefon
-                  </Text>
-                  <Text>{charInfo.phone ?? 'Okänt'}</Text>
-                </Stack>
-              </Grid.Col>
+                <Grid.Col span={4}>
+                  <Stack spacing={4}>
+                    <Text fz="xs" c="dimmed">
+                      Telefon
+                    </Text>
+                    <Text>{charInfo.phone ?? 'Okänt'}</Text>
+                  </Stack>
+                </Grid.Col>
 
-              <Grid.Col span={4}>
-                <Stack spacing={4} mt={8}>
-                  <Text fz="xs" c="dimmed">
-                    Födelsedag
-                  </Text>
-                  <Text>{charInfo.birthdate}</Text>
-                </Stack>
-              </Grid.Col>
-            </Grid>
+                <Grid.Col span={4}>
+                  <Stack spacing={4}>
+                    <Text fz="xs" c="dimmed">
+                      Födelsedag
+                    </Text>
+                    <Text>{charInfo.birthdate}</Text>
+                  </Stack>
+                </Grid.Col>
+              </Grid>
+            </Skeleton>
 					</Paper>
 				</Grid.Col>
 
 				<Grid.Col span={4}>
 					<Paper withBorder radius="md" p="xs">
-						<Group position="left">
+						<Group mb={8} position="left">
 							<IconCash size={24} />
 							<Title order={3} transform="uppercase">
 								tillgångar
 							</Title>
 						</Group>
 
-            <Grid grow>
-              <Grid.Col span={4}>
-                <Stack spacing={4} mt={8}>
-                  <Text fz="xs" c="dimmed">
-                    Namn
-                  </Text>
-                  <Text>{`${charInfo.firstname} ${charInfo.lastname}`}</Text>
-                </Stack>
-              </Grid.Col>
-            </Grid>
+            <Skeleton visible={isLoading}>
+              <Grid grow>
+                <Grid.Col span={4}>
+                  <Stack spacing={4}>
+                    <Text fz="xs" c="dimmed">
+                      Namn
+                    </Text>
+                    <Text>{`${charInfo.firstname} ${charInfo.lastname}`}</Text>
+                  </Stack>
+                </Grid.Col>
+              </Grid>
+            </Skeleton>
 					</Paper>
 				</Grid.Col>
 
 				<Grid.Col span={4}>
 					<Paper withBorder radius="md" p="xs">
-						<Group position="left">
+						<Group mb={8} position="left">
 							<IconChartAreaLine size={24} />
 							<Title order={3} transform="uppercase">
 								statistik
 							</Title>
 						</Group>
+
+            <Skeleton visible={isLoading}>
+              <Grid grow>
+                <Grid.Col span={4}>
+                  <Stack spacing={4}>
+                    <Text fz="xs" c="dimmed">
+                      Namn
+                    </Text>
+                    <Text>{`${charInfo.firstname} ${charInfo.lastname}`}</Text>
+                  </Stack>
+                </Grid.Col>
+              </Grid>
+            </Skeleton>
 					</Paper>
 				</Grid.Col>
 			</Grid>
