a29625d0b6c2a10d0cbbefce215f9802b425ad3b
Raven <theravenblu@gmail.com>
2023-02-05 05:19:21 +0100

    Remove webpack config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 const { withSentryConfig } = require('@sentry/nextjs');
-const SentryWebpackPlugin = require("@sentry/webpack-plugin");
 
 /** @type {import('next').NextConfig} */
 const moduleExports = {
@@ -11,23 +10,6 @@ const moduleExports = {
   sentry: {
     hideSourceMaps: true,
   },
-  devtool: "source-map", // Source map generation must be turned on
-  plugins: [
-    new SentryWebpackPlugin({
-      org: "dotit-ro",
-      project: "bygden-rp",
-
-      // Specify the directory containing build artifacts
-      include: "./dist",
-
-      // Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
-      // and needs the `project:releases` and `org:read` scopes
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-
-      // Optionally uncomment the line below to override automatic release name detection
-      // release: process.env.RELEASE,
-    }),
-  ],
 };
 
 const sentryWebpackPluginOptions = {
