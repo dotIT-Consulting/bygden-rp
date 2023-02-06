2cb490cdc2c278c2ab15e2c24d923184281d4c87
Raven <theravenblu@gmail.com>
2023-02-06 05:18:51 +0100

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,8 @@ import router from "@utils/libs/Router";
 import { useStore } from "@utils/libs/Zustand";
 import shallow from "zustand/shallow";
 import { ISteamResponse } from "@utils/Types";
-import  { SWRConfig } from 'swr'
+import  { SWRConfig } from 'swr';
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const SITE_URL = process.env.NODE_ENV === 'production' ? process.env.SITE_URL : 'http://localhost:3000/'
 
@@ -37,7 +38,7 @@ export default function App(props: AppProps & {
       <Head>
         <title>Bygden RP</title>
       </Head>
-
+      <GoogleAnalytics trackPageViews />
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
